9dc5a3375e7e8d72e590d66b793703a4473ed806
James Ryan <work.jdryan@gmail.com>
2022-01-26 12:04:40 +1100

    removed unused plugin file, moved import to init

--- src/affinder/__init__.py
+++ src/affinder/__init__.py
@@ -2,3 +2,5 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
+
+from .affinder import start_affinder
--- src/affinder/plugin.py
+++
@@ -1,2 +0,0 @@
-from napari_plugin_engine import napari_hook_implementation
-from .affinder import start_affinder
