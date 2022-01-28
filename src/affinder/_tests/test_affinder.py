7026409db819e92e0f70c910e110db2c86ecc6f0
James Ryan <work.jdryan@gmail.com>
2022-01-28 12:23:45 +1100

    refactoring code to fit best practice

--- src/affinder/_tests/test_affinder.py
+++ src/affinder/_tests/test_affinder.py
@@ -8,18 +8,18 @@ def test_basic(make_napari_viewer, tmp_path):
     image0 = data.camera()
     image1 = transform.rotate(image0[100:, 32:496], 60)
 
-    my_viewer = make_napari_viewer()
+    viewer = make_napari_viewer()
 
-    l0 = my_viewer.add_image(image0, colormap='green', blending='additive')
-    l1 = my_viewer.add_image(image1, colormap='magenta', blending='additive')
+    l0 = viewer.add_image(image0, colormap='green', blending='additive')
+    l1 = viewer.add_image(image1, colormap='magenta', blending='additive')
 
     my_widget_factory = start_affinder()
-    my_widget_factory(viewer=my_viewer, reference=l0, moving=l1, model=AffineTransformChoices.affine, output=tmp_path / 'my_affine.txt')
+    my_widget_factory(viewer=viewer, reference=l0, moving=l1, model=AffineTransformChoices.affine, output=tmp_path / 'my_affine.txt')
 
-    my_viewer.layers['image0_pts'].data = np.array([[148.19396647, 234.87779732],
+    viewer.layers['image0_pts'].data = np.array([[148.19396647, 234.87779732],
                                                  [484.56804381, 240.55720892],
                                                  [474.77521025, 385.88403205]])
-    my_viewer.layers['image1_pts'].data = np.array([[150.02534429, 80.65355322],
+    viewer.layers['image1_pts'].data = np.array([[150.02534429, 80.65355322],
                                                  [314.75696913, 375.13825634],
                                                  [184.33085012, 439.81718637]])
     actual = np.asarray(l1.affine)
