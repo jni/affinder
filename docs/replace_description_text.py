7381295f08dd96641dd69332d7e5bd4f1a043a94
Juan Nunez-Iglesias <jni@fastmail.com>
2023-10-11 18:14:05 +1100

    Docs build uses README instead of DESCRIPTION

--- docs/replace_description_text.py
+++ docs/replace_description_text.py
@@ -1,6 +1,6 @@
 """
-for ../.napari/DESCRIPTION.md
-find URLs ending in .mp4 and replace URL wrapped in HTML video tag 
+for README.md
+find URLs ending in .mp4 and replace it with URL wrapped in HTML video tag.
 """
 
 replacement_url = '<video width="640" height="480" controls>\n\
@@ -9,7 +9,7 @@ Your browser does not support the video tag.\n\
 </video>\n'
 new_text = ""
 
-with open(".napari/DESCRIPTION.md", 'r+') as desc_file:
+with open("README.md", 'r+') as desc_file:
     for line in desc_file:
         if line.strip().startswith("https://user-images.githubusercontent") and line.strip().endswith(".mp4"):
             line = replacement_url.format(line.strip())
